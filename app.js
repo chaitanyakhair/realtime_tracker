1cc6161cca2ef8c5e1909989f3bd3054b3138d7d
chaitanyakhair <chaitanyakhair@gmail.com>
2024-07-11 18:45:57 +0530

    added functionality

--- app.js
+++ app.js
@@ -1,15 +1,25 @@
-const express = require ("express");
+const express = require("express");
 const app = express();
 const http = require("http");
+const path = require("path");
 const socketio = require("socket.io")
 const server = http.createServer(app);
 const io = socketio(server);
 
 app.set("view engine","ejs");
-app.set(express.static(path.join(__dirname,"public")));
+app.use(express.static(path.join(__dirname,"public")));
+
+io.on("connection", function (socket) {
+    socket.on("send-location", function (data){
+        io.emit("recive-location",{id : socket.id , ...data});
+    });
+    socket.on("disconnect", function() {
+        io.emit("user-disconnected", socket.id)
+    });
+});
 
 app.get( "/", function ( req , res ){
-    res.send("hey")
+    res.render("index.ejs");
 }); 
 
 server.listen("3000");
